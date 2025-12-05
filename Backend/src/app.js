a24926389980442c8417cd027e25af1338fef9d1
Instagram-user-identity <mrpiyushchaudhari2006@gmail.com>
2025-12-05 22:36:21 +0530

    change cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const app =express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://piyushchaudhari-masalainsta.onrender.com",
+    origin:"*",
     credentials:true
 }))
 app.use('/api/auth',authRoutes)
